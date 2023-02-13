631eeaf799ea6a97871b9fd43bb89a54ad26c309
Anushka Wani <anushkan.wani@gmail.com>
2023-02-13 15:23:40 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function clickButton() {
+    let line = document.getElementById("parallel");
+    let color = line.style.stroke
+    
+    line.style.color;
+}
+
+document.getElementById("button1").addEventListener("click", clickButton);
